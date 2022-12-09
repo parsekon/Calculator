0b529ae5a3bcf8e377391905ccf8e9814c9c827b
DMITRIY <DMITRIY@MacBook-Pro-2.local>
2022-12-09 20:08:34 +0600

    Change Bloknot.js

--- client/src/components/Blocknot/Blocknot.js
+++ client/src/components/Blocknot/Blocknot.js
@@ -54,7 +54,7 @@ export default function Blocknot() {
     } catch (error) {
       console.error(error);
     }
-  }, [lastIndex, loaderButton]);
+  }, [lastIndex, arrayTasks, loaderButton, loaderLiTask]);
 
   const handleCompleteTaskClick = async (id) => {
     setLoaderLiTask(true);
